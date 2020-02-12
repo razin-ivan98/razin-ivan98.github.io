d6e5866c2309a5c88c9b967db29969ffc485eae1
razin-ivan98 <razin-ivan98@ya.ru>
2020-02-12 13:48:49 +0300

    rotation intagrated

--- fdf/main.js
+++ fdf/main.js
@@ -1,14 +1,15 @@
 var arr = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
 var width;
@@ -19,32 +20,80 @@ var selected = { x: 0, y: 0 };
 var canvas;
 var context;
 
-function clickCanvas(event) {
-    let clickX = event.layerX;
-    let clickY = event.layerY;
+var alpha = -1;
+var beta = 0;
+
+var prevX = 0;
+var prevY = 0;
+
+var is_rot = 0;
+
+function canvasMouseDown(event) {
+    is_rot = 1
+    let clickX = (event.offsetX === undefined) ? event.layerX : event.offsetX;
+    let clickY = (event.offsetY === undefined) ? event.layerY : event.offsetY;
+    prevX = clickX;
+    prevY = clickY;
+}
+function canvasTouchStart(event) {
+    is_rot = 1
+    let clickX = event.changedTouches[0].clientX;
+    let clickY = event.changedTouches[0].clientY;
+    prevX = clickX;
+    prevY = clickY;
+}
+function canvasMouseUp() {
+    is_rot = 0;
+}
+
+function canvasClick(event) {
+    let clickX = (event.offsetX === undefined) ? event.layerX : event.offsetX;
+    let clickY = (event.offsetY === undefined) ? event.layerY : event.offsetY;
 
     for (let i = 0; i < screen_arr.length; i++) {
         for (let j = 0; j < screen_arr[0].length; j++) {
-            if (clickX + 10 > screen_arr[i][j].x &&
-                clickX - 10 < screen_arr[i][j].x &&
-                clickY + 10 > screen_arr[i][j].y &&
-                clickY - 10 < screen_arr[i][j].y) {
+            if (clickX + 5 > screen_arr[i][j].x &&
+                clickX - 5 < screen_arr[i][j].x &&
+                clickY + 5 > screen_arr[i][j].y &&
+                clickY - 5 < screen_arr[i][j].y) {
                 selected.x = i;
                 selected.y = j;
                 redraw();
             }
         }
     }
+
+}
+function canvasMouseMove(event) {
+    if (is_rot == 1) {
+        let clickX = (event.offsetX === undefined) ? event.layerX : event.offsetX;
+        let clickY = (event.offsetY === undefined) ? event.layerY : event.offsetY;
+        let deltaX = clickX - prevX;
+        let deltaY = clickY - prevY;
+        prevX = clickX;
+        prevY = clickY;
+        alpha += deltaY * 0.05;
+        beta -= deltaX * 0.05;
+        recalc();
+        redraw();
+    }
+}
+function canvasTouchMove(event) {
+    if (is_rot == 1) {
+        let clickX = event.changedTouches[0].clientX;
+        let clickY = event.changedTouches[0].clientY;
+        let deltaX = clickX - prevX;
+        let deltaY = clickY - prevY;
+        prevX = clickX;
+        prevY = clickY;
+        alpha += deltaY * 0.05;
+        beta -= deltaX * 0.05;
+        recalc();
+        redraw();
+    }
 }
 
 window.onload = function () {
-
-    this.document.addEventListener('touchmove', function (event) {
-        if (event.scale !== 1) {
-            event.preventDefault();
-        }
-    });
-
 
     width = width = document.body.clientWidth;
     canvas = this.document.querySelector('#canvas');
@@ -55,8 +104,15 @@ window.onload = function () {
     else {
         this.width = 500;
     }
+
     context = canvas.getContext('2d');
-    canvas.onclick = this.clickCanvas;
+    canvas.onclick = this.canvasClick;
+    canvas.onmousemove = this.canvasMouseMove;
+    this.canvas.onmousedown = this.canvasMouseDown;
+    this.canvas.onmouseup = this.canvasMouseUp;
+    this.canvas.ontouchstart = this.canvasTouchStart;
+    this.canvas.ontouchend = this.canvasMouseUp;
+    this.canvas.ontouchmove = this.canvasTouchMove;
     this.recalc();
     this.redraw();
 }
@@ -95,24 +151,46 @@ function printSelected() {
     context.fill();
 }
 
-function point_proj(x, y, z) {
+function pointRotateX(x, y, z) {
+    let point = {
+        x: x,
+        y: y * Math.cos(alpha) + z * Math.sin(alpha),
+        z: y * (-Math.sin(alpha)) + z * Math.cos(alpha)
+    };
+    return point;
+}
+function pointRotateY(x, y, z) {
+    let point = {
+        x: x * Math.cos(beta) + z * (-Math.sin(beta)),
+        y: y,
+        z: x * Math.sin(beta) + z * Math.cos(beta)
+    };
+    return point;
+}
+
+function pointProj(x, y, z) {
 
     x -= arr[0].length / 2;
     y -= arr.length / 2;
-    x *= width / 6.5;
-    y *= width / 6.5;
-    z *= width / 6.5;
+    x = x * width / 15;
+    y = y * width / 15;
+    z = z * width / 15;
 
     // let point = {
     //     x: (x - y) * Math.sqrt(3) / 2 + 250,
     //     y: -z + (x + y) / 2 + 250
     // };
 
-    let h = 1000 - (z / 2 + (x + y) * 0.612);
+    let r_p = pointRotateX(x, y, z);
+    r_p = pointRotateY(r_p.x, r_p.y, r_p.z);
+
+    //let h = 1000 - (r_p.z / 2 + (r_p.x + r_p.y) * 0.612);
 
     let point = {
-        x: 1000 / (1000 + h) * (x - y) * 0.707 + width / 2,
-        y: 1000 / (1000 + h) * ((x + y) * 0.35 - z * 0.866) + width / 1.5
+        // x: 1000 / (1000 + h) * (r_p.x - r_p.y) * 0.707 + width / 2,
+        // y: 1000 / (1000 + h) * ((r_p.x + r_p.y) * 0.35 - r_p.z * 0.866) + width / 1.5
+        x: r_p.x * (1000.0 / (1000 - r_p.z)) + width / 2,
+        y: r_p.y * (1000.0 / (1000 - r_p.z)) + width / 2
     }
     return point;
 }
@@ -130,7 +208,7 @@ function minus() {
 
 function recalc(mode) {
     if (mode === 'selected') {
-        screen_arr[selected.x][selected.y] = point_proj(selected.y, selected.x, arr[selected.x][selected.y]);
+        screen_arr[selected.x][selected.y] = pointProj(selected.y, selected.x, arr[selected.x][selected.y]);
     }
     else {
         let first;
@@ -138,10 +216,21 @@ function recalc(mode) {
         for (let i = 0; i < arr.length; i++) {
             let row = [];
             for (let j = 0; j < arr[0].length; j++) {
-                first = point_proj(j, i, arr[i][j]);
+                first = pointProj(j, i, arr[i][j]);
                 row.push(first);
             }
             this.screen_arr.push(row);
         }
     }
+}
+
+function rotX(slider) {
+    alpha = slider.value * Math.PI;
+    recalc();
+    redraw();
+}
+function rotY(slider) {
+    beta = slider.value * Math.PI;
+    recalc();
+    redraw();
 }
