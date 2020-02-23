683d1b131b546ab8923c86d3724ff73a820ec3f9
razin-ivan98 <razin-ivan98@ya.ru>
2020-02-23 19:51:43 +0300

    julia

--- julia/handling.js
+++ julia/handling.js
@@ -86,20 +86,20 @@ function canvasTouchMove(event) {
         let y = event.targetTouches[0].clientY;
         let deltaX = x - prevX;
         let deltaY = y - prevY;
-        let x2 = event.changedTouches[1].clientX;
-        let y2 = event.changedTouches[1].clientY;
-        let deltaX2 = x2 - prevX2;
-        let deltaY2 = y2 - prevY2;
+        // let x2 = event.targetTouches[1].clientX;
+        // let y2 = event.targetTouches[1].clientY;
+        // let deltaX2 = x2 - prevX2;
+        // let deltaY2 = y2 - prevY2;
         prevX = x;
         prevY = y;
-        prevX2 = x2;
-        prevY2 = y2;
+        // prevX2 = x2;
+        // prevY2 = y2;
 
         delta[0] -= 0.008 * deltaX / scale;
         delta[1] += 0.008 * deltaY / scale;
 
-        scale += 0.008 * Math.sqrt(pow(deltaX + deltaX2, 2) + pow(deltaY + deltaY2, 2));
-        if (scale < 1)
-            scale = 1;
+        // scale += 0.008 * Math.sqrt(pow(deltaX + deltaX2, 2) + pow(deltaY + deltaY2, 2));
+        // if (scale < 1)
+        //     scale = 1;
     }
 }
